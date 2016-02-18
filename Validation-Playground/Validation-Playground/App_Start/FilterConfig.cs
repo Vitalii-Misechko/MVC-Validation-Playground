7146d550e671a9f66383b150c83428c2fad50d3c
Vitalii Misechko <Vitalii.Misechko@D2L.com>
2016-02-18 11:07:55 +0200

    Committing template

---
+++ Validation-Playground/Validation-Playground/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Validation_Playground {
+	public class FilterConfig {
+		public static void RegisterGlobalFilters( GlobalFilterCollection filters ) {
+			filters.Add( new HandleErrorAttribute() );
+		}
+	}
+}
