a7537def04df9e4cc5f77c5cfa9a91a5ef6a116b
Vitalii Misechko <Vitalii.Misechko@D2L.com>
2016-02-18 17:39:00 +0200

    Added jQuery datapicker. Fixed formatting issue

--- Validation-Playground/Validation-Playground/Models/Movie.cs
+++ Validation-Playground/Validation-Playground/Models/Movie.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
+using System.Web.Mvc;
 
 namespace Validation_Playground.Models {
 
+	[Bind( Include = "ID,Title,ReleaseDate,Genre,Price" )]
 	public class Movie {
 
 		public int ID { get; set; }
+
 		public string Title { get; set; }
+
+		[DisplayName("Release Date")]
+		[DisplayFormat( ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}" )]
+
 		public DateTime ReleaseDate { get; set; }
+
 		public string Genre { get; set; }
+
 		public decimal Price { get; set; }
 
 	}
